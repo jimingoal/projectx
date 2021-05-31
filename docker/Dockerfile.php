070c0420310fac6f958dd81de58bdd24e4371323
flexboni <kgb8594@gmail.com>
2021-06-01 00:01:43 +0900

    Docker Compose - PHP MySQLi 모듈 추가하기

---
+++ docker/Dockerfile.php
@@ -0,0 +1,8 @@
+# 설치할 PHP 버전
+FROM php:8.0.5-fpm-alpine
+
+# RUN docker-php-ext-install 다음에 설치하고자 하는 모듈을 차례로 적는다.
+RUN docker-php-ext-install mysqli
+
+# 환경 설정 파일을 Production 의 것으로 복사한다.
+RUN cp "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
