856019ed7f49320548272d238ecccb3a83f99bf7
flexboni <kgb8594@gmail.com>
2021-06-06 23:32:49 +0900

    home 파일에 api 연동 로직 적용
    - http 패키지 추가
    - api 통신 후 가져온 데이터로 리스트 보여주는 로직 적용

---
+++ simple_flutter/lib/screens/home.dart
@@ -0,0 +1,83 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../env.sample.dart';
+import '../models/student.dart';
+import './details.dart';
+import './create.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late Future<List<Student>> students;
+  final studentListKey = GlobalKey<_HomeState>();
+
+  @override
+  void initState() {
+    super.initState();
+    students = getStudentsList();
+  }
+
+  Future<List<Student>> getStudentsList() async {
+    final response = await http.get(Uri.parse("${Env.URL_PREFIX}/list.php"));
+    return json
+        .decode(response.body)
+        .cast<Map<String, dynamic>>()
+        .map<Student>((json) => Student.fromJson(json))
+        .toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: studentListKey,
+      appBar: AppBar(title: Text('Student List')),
+      body: Center(
+        child: FutureBuilder<List<Student>>(
+          future: students,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            // By default, show a loading spinner.
+            if (!snapshot.hasData) return CircularProgressIndicator();
+
+            // Render student lists
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                var data = snapshot.data[index];
+                return Card(
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    trailing: Icon(Icons.view_list),
+                    title: Text(
+                      data.name,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Details(student: data),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (_) => Create()),
+        ),
+      ),
+    );
+  }
+}
