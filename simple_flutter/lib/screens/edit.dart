dd7f830b3acc2cb48075b4ed675605d5f9ec27ac
flexboni <kgb8594@gmail.com>
2021-06-06 23:31:22 +0900

    screens 폴더에 화면별 파일 생성

---
+++ simple_flutter/lib/screens/edit.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Edit extends StatelessWidget {
+  const Edit({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
