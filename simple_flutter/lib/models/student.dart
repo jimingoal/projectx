af9f4f3d87d026478baf36f0c86d5c8b51120988
flexboni <kgb8594@gmail.com>
2021-06-06 23:31:39 +0900

    Student VO 생성

---
+++ simple_flutter/lib/models/student.dart
@@ -0,0 +1,20 @@
+class Student {
+  final int id;
+  final String name;
+  final int age;
+
+  Student({required this.id, required this.name, required this.age});
+
+  factory Student.fromJson(Map<String, dynamic> json) {
+    return Student(
+      id: json['id'],
+      name: json['name'],
+      age: json['age'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    'age': age,
+  };
+}
